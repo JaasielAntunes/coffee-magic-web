86451e0130d75a351b392f6e775b795573d8a5c7
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-15 11:48:43 -0300

    Loading state no perfil

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,6 @@ import { z } from 'zod'
 export const env = z
   .object({
     VITE_API_URL: z.string().url(),
+    VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
   })
   .parse(import.meta.env)
