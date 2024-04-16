6cc64f529f6a12983eea122326e8e7e25ae89c9b
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-16 11:28:54 -0300

    Mock de autenticação

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const env = z
   .object({
-    VITE_API_URL: z.string().url(),
+    VITE_API_URL: z.string(),
     VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
     MODE: z.enum(['production', 'development', 'test']),
   })
