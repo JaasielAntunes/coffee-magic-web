57b5150df9777c022932dd94bda4f7c4d389f813
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-16 11:14:07 -0300

    Setup do MSW

--- src/env.ts
+++ src/env.ts
@@ -4,5 +4,6 @@ export const env = z
   .object({
     VITE_API_URL: z.string().url(),
     VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
+    MODE: z.enum(['production', 'development', 'test']),
   })
   .parse(import.meta.env)
