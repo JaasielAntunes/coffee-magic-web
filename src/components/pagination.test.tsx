ff9b0ec4c1209732e96e79e55e29df8f9cd0f539
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-16 10:44:34 -0300

    Utilizando Spies no teste de paginação

---
+++ src/components/pagination.test.tsx
@@ -0,0 +1,43 @@
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { Pagination } from './pagination'
+
+const onPageChangeCallback = vi.fn()
+
+describe('Paginação', () => {
+  test('deve exibir a quantidade certa de páginas e resultados', () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={() => {}}
+      />,
+    )
+
+    expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
+    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  })
+
+  test('deve ser capaz de navegar para a próxima página', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(1)
+  })
+})
