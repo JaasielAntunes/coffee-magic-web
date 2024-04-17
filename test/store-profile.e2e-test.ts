f24b18ab61c2f0c69197b8e09825aa5e8fbbcba7
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-17 14:25:07 -0300

    Teste E2E de edição de perfil

---
+++ test/store-profile.e2e-test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('atualização de perfil com sucesso', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Coffee Magic' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Rocket Café')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(2000)
+
+  expect(page.getByRole('button', { name: 'Rocket Café' })).toBeVisible()
+})
